a8e7b79682009a299c4f15f842f9aedb1fad5c91
Andrei <garnash1313@gmail.com>
2024-08-07 12:54:49 +0300

    Initial Commit

---
+++ Instagram (SwiftUI)/Instagram__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram__SwiftUI_App.swift
+//  Instagram (SwiftUI)
+//
+//  Created by Andrei Harnashevich on 7.08.24.
+//
+
+import SwiftUI
+
+@main
+struct Instagram__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
